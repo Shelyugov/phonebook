e9b0d54b6ef2e8bdf54b3ad0a1527134dfafc5a6
A.Shelyugov <mr.samuel777@gmail.com>
2017-03-22 16:21:38 +0300

    Improve HTTP controller

--- phonebook-web/src/main/java/ru/alexander/phonebook/web/rest/PhonebookHttpController.java
+++ phonebook-web/src/main/java/ru/alexander/phonebook/web/rest/PhonebookHttpController.java
@@ -1,9 +1,11 @@
 package ru.alexander.phonebook.web.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import ru.alexander.phonebook.entity.PersonalData;
+import ru.alexander.phonebook.jdbc.JdbcRepository;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
 /**
  * Контроллер телефонной книги для обработки HTTP-запросов.
@@ -11,10 +13,44 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class PhonebookHttpController {
 
+//    private final Repository repository = new Repository();
+
+    private final JdbcRepository repository = new JdbcRepository();
+
     @GET
+    @Path("/helloWorld")
     @Produces(MediaType.TEXT_PLAIN)
     public String helloWorld() {
         return "Hello, world!";
     }
 
+    @PUT
+    @Path("/addNewPersonalData")
+    public String addNewPersonalData() {
+        final PersonalData pd = new PersonalData();
+        //try {
+            pd.setName(UUID.randomUUID().toString());
+            pd.setSurname(UUID.randomUUID().toString());
+            if (repository.save(pd)) {
+                return "Successfully added " + pd;
+            } else {
+                return "Failed to save " + pd;
+            }
+        //} catch (Exception e) {
+            //return e.getMessage();
+        //}
+    }
+
+    @DELETE
+    @Path("/deletePersonalData")
+    public String deletePersonalData(@DefaultValue("0") @QueryParam("id") final long id) {
+        if (id <= 0) {
+            return "Please, specify the required identifier of personal data with \"id\" parameter.";
+        }
+        if (repository.delete(id)) {
+            return "Successfully deleted row with id " + id + "!";
+        } else {
+            return "Failed to delete row with id" + id + ".";
+        }
+    }
 }
