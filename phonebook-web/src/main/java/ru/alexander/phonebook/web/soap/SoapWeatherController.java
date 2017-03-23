73cfcb4c0f1b9fa59529a2ff4f99bbfa87c6c684
A.Shelyugov <mr.samuel777@gmail.com>
2017-03-23 19:02:52 +0300

    Fix weather SOAP service

--- phonebook-web/src/main/java/ru/alexander/phonebook/web/soap/SoapWeatherController.java
+++ phonebook-web/src/main/java/ru/alexander/phonebook/web/soap/SoapWeatherController.java
@@ -1,12 +1,15 @@
 package ru.alexander.phonebook.web.soap;
 
 import net.webservicex.GlobalWeather;
+import net.webservicex.GlobalWeatherSoap;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 /**
  * SOAP-контроллер, использующий внешний soap-сервис.
+ *
  * @see <a href="http://www.webservicex.com/New/Home/ServiceDetail/56">GlobalWeather service description</a>
  * @see <a href="http://www.webservicex.com/globalweather.asmx?wsdl">GlobalWeather WSDL file</a>
  */
@@ -14,11 +17,19 @@ import javax.jws.WebService;
 @WebService
 public class SoapWeatherController {
 
+    private static final String COUNTRY = "Russia";
+
     private final GlobalWeather soap = new GlobalWeather();
 
     @WebMethod
-    public String getGlobalWeatherSoap() {
-        //return soap.getGlobalWeatherSoap();
-        return "Weather API's coming up!";
+    public String getWeather(@WebParam(name = "city") String city) {
+        final GlobalWeatherSoap service = soap.getGlobalWeatherSoap();
+        return service.getWeather(city, COUNTRY);
+    }
+
+    @WebMethod
+    public String getCities() {
+        final GlobalWeatherSoap service = soap.getGlobalWeatherSoap();
+        return service.getCitiesByCountry(COUNTRY);
     }
 }
