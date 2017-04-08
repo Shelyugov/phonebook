70948e32491fcd6ea4394129c41b763a029559b3
Shelyugov <mr.samuel777@gmail.com>
2017-04-08 14:02:51 +0300

    Add date parameter to HTTP GET external/currency method

--- phonebook-web/src/main/java/ru/alexander/phonebook/web/rest/ExternalHTTPService.java
+++ phonebook-web/src/main/java/ru/alexander/phonebook/web/rest/ExternalHTTPService.java
@@ -1,7 +1,9 @@
 package ru.alexander.phonebook.web.rest;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -14,16 +16,26 @@ import javax.ws.rs.core.Response;
  */
 @Path("/external")
 public class ExternalHTTPService {
+    /**
+     * URL запроса
+     */
+    private static final String RESOURCE_URI = "http://api.fixer.io";
 
-    private static final String RESOURCE_URI = "http://api.fixer.io/latest";
+    /**
+     * Валюта по умолчанию
+     */
     private static final String ROUBLES = "RUB";
+
+    /**
+     * Дата по умолчанию
+     */
+    private static final String DEFAULT_DATE = "latest";
 
     @GET
     @Path("/currency")
-    public Response getCurrency() {
+    public Response getCurrency(@PathParam("date") @DefaultValue(DEFAULT_DATE) String date) {
         final Client client = ClientBuilder.newClient();
-        final WebTarget target = client.target(RESOURCE_URI).queryParam("base", ROUBLES);
+        final WebTarget target = client.target(RESOURCE_URI).path(date).queryParam("base", ROUBLES);
         return target.request().get();
     }
-
 }
